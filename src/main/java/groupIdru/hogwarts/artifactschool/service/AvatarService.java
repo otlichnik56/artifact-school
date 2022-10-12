8f160d37f0a554d06c53f0f2a3c7d94f02e27c26
Hyp <otlichnik56@yandex.ru>
2022-10-13 00:37:50 +0500

    Домашка 3.5 от 12.10.2022 сырая, доработки нужны и без упрощенной графики

--- src/main/java/groupIdru/hogwarts/artifactschool/service/AvatarService.java
+++ src/main/java/groupIdru/hogwarts/artifactschool/service/AvatarService.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
 import javax.transaction.Transactional;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -58,5 +61,22 @@ public class AvatarService {
     private String getExtension(String fileName) {
         return fileName.substring(fileName.indexOf(".") + 1);
     }
+
+    public byte[] generateImageData(Path filePath) throws IOException{
+        try (InputStream is = Files.newInputStream(filePath);
+             BufferedInputStream bis = new BufferedInputStream(is, 1024);
+             ByteArrayOutputStream baos = new ByteArrayOutputStream())
+            {
+                BufferedImage image = ImageIO.read(bis);
+                int height = image.getHeight() / (image.getWidth() / 100);
+                BufferedImage data = new BufferedImage(100, height, image.getType());
+                Graphics2D graphics = data.createGraphics();
+                graphics.drawImage(image, 0, 0, Color.blue, null);
+                graphics.dispose();
+                ImageIO.write(data, getExtension(filePath.getFileName().toString()), baos);
+                return baos.toByteArray();
+            }
+    }
+
 
 }
