d6997d31e30df5e55685b0974ea3297349cd4856
Hyp <otlichnik56@yandex.ru>
2022-10-19 19:35:55 +0500

    Домашка 4.2 от 19.10.2022

--- src/main/java/groupIdru/hogwarts/artifactschool/scripts422.sql
+++ src/main/java/groupIdru/hogwarts/artifactschool/scripts422.sql
@@ -1,19 +1,24 @@
-CREATE TABLE students(
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    age INTEGER,
-    faculty_id INTEGER REFERENCES faculties(id)
+CREATE TABLE peoples(
+    id             INTEGER PRIMARY KEY,
+    name           TEXT,
+    age            INTEGER,
+    driver_license BOOLEAN,
+    car_id         INTEGER REFERENCES cars(id)
 );
 
-CREATE TABLE faculties(
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    color TEXT
+CREATE TABLE cars(
+   id       INTEGER PRIMARY KEY,
+   brand    TEXT,
+   model    TEXT,
+   price    REAL
 );
+
+
 
 SELECT students.name, students.age, faculties.name
     FROM students
-    INNER JOIN faculties ON students.faculty_id = faculties.id;
+    LEFT JOIN faculties ON students.faculty_id = faculties.id;
+
 
 SELECT students.name, students.age, avatar.file_path
     FROM avatar
